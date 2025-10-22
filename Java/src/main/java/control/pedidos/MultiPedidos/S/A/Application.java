692761f60d901415847ae50f87edfe4776bbc733
Ronaldo <rmonzon@homeland.com.gt>
2025-10-22 14:25:09 -0600

    Inicio

---
+++ Java/src/main/java/control/pedidos/MultiPedidos/S/A/Application.java
@@ -0,0 +1,13 @@
+package control.pedidos.MultiPedidos.S.A;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
