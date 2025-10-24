d49a7a91f14437d8a32fed3b8efbe54994504106
Ronaldo <rmonzon@homeland.com.gt>
2025-10-23 22:43:39 -0600

    Creacion proyectos

---
+++ Backend/spring-mariadb/src/main/java/com/multipedidos/mariadb/MariaDBApplication.java
@@ -0,0 +1,12 @@
+package com.multipedidos.mariadb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MariaDBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MariaDBApplication.class, args);
+    }
+}
